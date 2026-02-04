300338d8a05ed937f81cd9f6f53c437a4c7d76b3
Abdullah Abbad <codegenius.inc@gmail.com>
2026-02-04 22:29:13 +0500

    update contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import Header from '@/components/layout/Header'
-import Footer from '@/components/layout/Footer'
-import ContactUs from '@/components/contact/ContactUs'
-import TrustBadges from '@/components/home/TrustBadges'
-import GetInTouch from '@/components/home/GetInTouch'
+import React from "react";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import ContactUs from "@/components/contact/ContactUs";
+import TrustBadges from "@/components/home/TrustBadges";
 
 const Contact = () => {
   return (
-        <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-1">
         <ContactUs />
         <TrustBadges />
-        <GetInTouch />
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
